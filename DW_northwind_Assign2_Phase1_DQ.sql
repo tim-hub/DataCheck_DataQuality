8f4956e567a333b925685e372b8e7a85b251a8c1
tim-hub <tim_w@outlook.com>
2018-05-08 14:12:25 +1200

    finish db 7 checking

--- DW_northwind_Assign2_Phase1_DQ.sql
+++ DW_northwind_Assign2_Phase1_DQ.sql
@@ -64,7 +64,7 @@ Insert DQLog
 print '================ BEGIN RULE ## CHECKING =================='
 print 'DQ Rule 2: 	Quantity Check'
 print 'Action: 		Allow'
-print 'Database: 	noethwind7'
+print 'Database: 	northwind7'
 print '------------------------'
 print 'Table: 		Order Details'
 print '------------------------'
@@ -77,7 +77,7 @@ Insert DQLog
 print '================ BEGIN RULE ## CHECKING =================='
 print 'DQ Rule 3: 	Discount Check'
 print 'Action: 		Allow'
-print 'Database: 	noethwind7'
+print 'Database: 	northwind7'
 print '------------------------'
 print 'Table: 		Order Details'
 print '------------------------'
@@ -86,6 +86,49 @@ Insert DQLog
 	SELECT od.%%physloc%%, 'northwind7', 'Order Details', 3, 'Allow'
 	From northwind7.dbo.[Order Details] od, northwind7.dbo.Products p
 	WHERE od.Discount >0.2 and od.ProductID=p.ProductID and p.UnitPrice>200
+
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 4: 	Country Check'
+print 'Action: 		Fix'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Customers, Suppliers'
+print '------------------------'
+
+-- declare this variable table to store irregaular country name
+DECLARE @irregularCountryList TABLE (name_wrong_format nvarchar(32),  name_right_format nvarchar(32))
+INSERT INTO @irregularCountryList
+SELECT name_int, name_out FROM (
+	VALUES ('US', 'USA'), ('United States', 'USA'), ('UNITED STATES', 'USA'),
+		 ('United Kingdom', 'UK'), ('UNITED KINGDOM', 'UK'), ('Britain', 'UK'), ('BRITAIN', 'UK')
+	
+	) AS tbl(name_int, name_out)
+-- SELECT * from @irregularCountryList
+
+Insert DQLog
+	SELECT c.%%physloc%%, 'northwind7', 'Customers', 4, 'Fix'
+	From northwind7.dbo.Customers c
+	WHERE c.Country in (Select name_wrong_format from @irregularCountryList)
+
+Insert DQLog
+	SELECT s.%%physloc%%, 'northwind7', 'Suppliers', 4, 'Fix'
+	From northwind7.dbo.Suppliers s
+	WHERE s.Country in (Select name_wrong_format from @irregularCountryList)
+
+
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 5: 	Postcode Check'
+print 'Action: 		Fix'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Customers'
+print '------------------------'
+
+Insert DQLog
+	SELECT c.%%physloc%%, 'northwind7', 'Customers', 5, 'Fix'
+	From northwind7.dbo.Customers c
+	WHERE LEN(c.PostalCode ) < 6
+
 print '================ BEGIN RULE ## CHECKING =================='
 print 'DQ Rule 6: 	Reject if categoryid or supplier id not exisits'
 print 'Action: 		Reject'
@@ -107,13 +150,76 @@ Insert DQLog
 
 
 print '================ BEGIN RULE ## CHECKING =================='
-print 'DQ Rule ##: 	######################'
-print 'Action: 		#####'
-print 'Database: 	#####'
+print 'DQ Rule 7: 	Check product id in Order Details'
+print 'Action: 		Reject'
+print 'Database: 	northwind7'
 print '------------------------'
-print 'Table: 		#####'
+print 'Table: 		Order Details'
 print '------------------------'
+Insert DQLog
+	SELECT od.%%physloc%%, 'northwind7', 'Order Details', 7, 'Reject'
+	From northwind7.dbo.[Order Details] od
+	WHERE od.ProductID = null or
+		od.ProductID not in (
+			Select ProductID From northwind7.dbo.Products
+		) 
 
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 8: 	Check Customer Id, ship Address, ship city'
+print 'Action: 		Reject'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Orders'
+print '------------------------'
+Insert DQLog
+	SELECT o.%%physloc%%, 'northwind7', 'Orders', 8, 'Reject'
+	From northwind7.dbo.[Orders] o
+	WHERE (o.ShipAddress = null and o.ShipCity=null) or
+		o.CustomerID= null or
+		o.CustomerID not in (
+			Select CustomerID From northwind7.dbo.Customers
+		) 
+
+
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 9: 	Check ShipVia'
+print 'Action: 		Allow'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Orders'
+print '------------------------'
+Insert DQLog
+	SELECT o.%%physloc%%, 'northwind7', 'Orders', 9, 'Allow'
+	From northwind7.dbo.[Orders] o
+	WHERE o.ShipVia = null or
+		o.ShipVia not in (
+			Select ShipperID from northwind7.dbo.Shippers
+		)
+
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 10: 	Check Freight'
+print 'Action: 		Reject'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Orders'
+print '------------------------'
+Insert DQLog
+	SELECT o.%%physloc%%, 'northwind7', 'Orders', 10, 'Reject'
+	From northwind7.dbo.[Orders] o, northwind7.dbo.[Order Details] od
+	WHERE od.OrderID = o.OrderID and o.Freight > 0.15 * (1-od.Discount)* od.Quantity * od.UnitPrice
+
+
+print '================ BEGIN RULE ## CHECKING =================='
+print 'DQ Rule 11: 	Check ShipDate'
+print 'Action: 		Fix'
+print 'Database: 	northwind7'
+print '------------------------'
+print 'Table: 		Orders'
+print '------------------------'
+Insert DQLog
+	SELECT o.%%physloc%%, 'northwind7', 'Orders', 11, 'Fix'
+	From northwind7.dbo.[Orders] o
+	WHERE o.ShippedDate = null
 
 print '=============== END RULE ## CHECKING ===================='
 
